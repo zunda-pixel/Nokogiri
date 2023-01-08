0cc2b4e10e6369f8dc78fd9c287e81722775fd0a
zunda <zunda.dev@gmail.com>
2023-01-08 21:05:17 +0900

    Initial Commit

---
+++ Nokogiri/NokogiriApp.swift
@@ -0,0 +1,17 @@
+//
+//  NokogiriApp.swift
+//  Nokogiri
+//
+//  Created by zunda on 2023/01/08.
+//
+
+import SwiftUI
+
+@main
+struct NokogiriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

bdc19fceba2dd96659a17772ebcc262f361e5ec7
zunda <zunda.dev@gmail.com>
2023-01-08 21:22:18 +0900

    add ci_post_clone.sh

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_post_clone.sh .sh
+#  Nokogiri
+#
+#  Created by zunda on 2023/01/08.
+#
+
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES

d664253c381ef2a603991b1d0c5cfce0dda7bd2c
zunda <zunda.dev@gmail.com>
2023-01-09 11:20:57 +0900

    comment out

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,4 +6,4 @@
 #  Created by zunda on 2023/01/08.
 #
 
-defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+# defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
